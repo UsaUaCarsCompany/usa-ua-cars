3735c9960bcae78c3808e2283932f0f5e4c9a77b
Yura <linetsky.yura@gmail.com>
2024-01-09 01:58:28 +0100

    fix favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,13 +26,13 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#202124" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/logo/favicon-16x16.png" />
+        <link rel="manifest" href="/logo/site.webmanifest" />
+        <link rel="mask-icon" href="/logo/safari-pinned-tab.svg" color="#202124" />
         <meta name="msapplication-TileColor" content="#202124" />
-        <meta name="theme-color" content="#ffffff"></meta>
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         <div className="wrapper">
