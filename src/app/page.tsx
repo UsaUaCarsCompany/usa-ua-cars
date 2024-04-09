7f0044f72aa723e2d3d478ba8172cb7326b51e6b
Yura <linetsky.yura@gmail.com>
2024-04-09 21:20:51 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+// HOME PAGE
+
 import CarCustomsCalculator from '@/components/Calculate/page'
 import HowWeWork from '@/components/HowWeWork/page'
 import InterestIn from '@/components/InterestIn/page'
@@ -8,14 +10,48 @@ import { VideoPlayer } from '@/components/VideoPlayer/page'
 import { client } from '../../sanity/lib/client'
 import { CarsDataProps } from '@/data/CarsData'
 import { VideosReviewsDataProps } from '@/data/VideosReviewsData'
-import { GetServerSideProps } from 'next'
 
-interface PropsHomeGetsData {
-  CarsDataCMS: CarsDataProps[]
-  VideosDataCMS: VideosReviewsDataProps[]
+export async function getCars() {
+  const query = `
+  *[_type == "cars"] | order(createdAt desc) {
+    id,
+    title,
+    "image": {
+      "alt": image.alt,
+      "_ref": image.asset._ref
+    },
+    price
+  }
+  `
+  const data = await client.fetch(query)
+
+  return data
 }
 
-const Home = ({ CarsDataCMS, VideosDataCMS }: PropsHomeGetsData) => {
+export async function getVideos() {
+  const query = `
+  *[_type == "videoReview"] | order(createdAt desc) {
+    id,
+    title,
+    subtitle,
+    "preview": {
+      "alt": preview.alt,
+      "_ref": preview.asset._ref
+    },
+    urlVideo
+  }
+  `
+  const data = await client.fetch(query)
+
+  return data
+}
+
+export const revalidate = 60
+
+const Home = async () => {
+  const CarsDataCMS: CarsDataProps[] = await getCars()
+  const VideosDataCMS: VideosReviewsDataProps[] = await getVideos()
+
   return (
     <main className="content">
       <Intro CarsDataCMS={CarsDataCMS} />
@@ -23,52 +59,10 @@ const Home = ({ CarsDataCMS, VideosDataCMS }: PropsHomeGetsData) => {
       <HowWeWork />
       <InterestIn />
       <PopularCars CarsDataCMS={CarsDataCMS} />
-      {VideosDataCMS.length == 0 ? '' : <VideoPlayer VideosDataCMS={VideosDataCMS} />}
+      {VideosDataCMS.length === 0 ? '' : <VideoPlayer VideosDataCMS={VideosDataCMS} />}
       <Questions />
     </main>
   )
-}
-
-export const getServerSideProps: GetServerSideProps<PropsHomeGetsData> = async () => {
-  const getCars = async () => {
-    const query = `
-    *[_type == "cars"] | order(createdAt desc) {
-      id,
-      title,
-      "image": {
-        "alt": image.alt,
-        "_ref": image.asset._ref
-      },
-      price
-    }
-    `
-    const data = await client.fetch(query)
-
-    return data
-  }
-
-  const getVideos = async () => {
-    const query = `
-    *[_type == "videoReview"] | order(createdAt desc) {
-      id,
-      title,
-      subtitle,
-      "preview": {
-        "alt": preview.alt,
-        "_ref": preview.asset._ref
-      },
-      urlVideo
-    }
-    `
-    const data = await client.fetch(query)
-
-    return data
-  }
-
-  const CarsDataCMS: CarsDataProps[] = await getCars()
-  const VideosDataCMS: VideosReviewsDataProps[] = await getVideos()
-
-  return { props: { CarsDataCMS, VideosDataCMS } }
 }
 
 export default Home
